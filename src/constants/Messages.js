ca9ae74cdfa9f80a4630adbdde51919105b4d816
suucong <suucong2@swu.ac.kr>
2025-11-02 22:33:50 +0900

    chore(message): 당첨 번호 및 보너스 번호 관련 에러 메시지 상수 정의

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -17,5 +17,16 @@ export const ERROR = Object.freeze({
   // 로또 검증 에러
   INVALID_COUNT: `${ERROR_PREFIX} 로또 번호는 6개여야 합니다.`,
   INVALID_RANGE: `${ERROR_PREFIX} 로또 번호는 1부터 45 사이의 숫자여야 합니다.`,
-  DUPLICATE_NUMBERS: `${ERROR_PREFIX} 당첨 번호는 중복되지 않아야 합니다.`,
+  DUPLICATE_NUMBERS: `${ERROR_PREFIX} 로또 번호는 중복되지 않아야 합니다.`,
+
+  // 당첨 번호 입력 에러
+  WINNING_INVALID_FORMAT: `${ERROR_PREFIX} 당첨 번호는 쉼표(,)로 구분된 숫자만 허용됩니다.`,
+  WINNING_INVALID_COUNT: `${ERROR_PREFIX} 당첨 번호는 6개여야 합니다.`,
+  WINNING_INVALID_RANGE: `${ERROR_PREFIX} 당첨 번호는 1부터 45 사이의 숫자여야 합니다.`,
+  WINNING_DUPLICATE_NUMBERS: `${ERROR_PREFIX} 당첨 번호는 중복되지 않아야 합니다.`,
+
+  // 보너스 번호 입력 에러
+  BONUS_NOT_NUMBER: `${ERROR_PREFIX} 보너스 번호는 숫자 1개만 입력해야 합니다.`,
+  BONUS_RANGE: `${ERROR_PREFIX} 보너스 번호는 1부터 45 사이의 숫자여야 합니다.`,
+  BONUS_DUPLICATE_WINNING: `${ERROR_PREFIX} 보너스 번호는 당첨 번호와 중복될 수 없습니다.`,
 });
