50d617aa97c0f19252405f838ca8f82df5ce83ba
suucong <suucong2@swu.ac.kr>
2025-11-03 23:15:09 +0900

    test(validator): PurchaseAmountValidator 테스트 코드 추가

---
+++ __tests__/validator/PurchaseAmountValidatorTest.js
@@ -0,0 +1,22 @@
+import { ERROR } from "../../src/constants/Messages.js";
+import PurchaseAmountValidator from "../../src/validator/PurchaseAmountValidator.js";
+
+describe("로또 클래스 테스트", () => {
+  test("로또 구입 금액이 숫자가 아닐 경우 예외가 발생한다.", () => {
+    expect(() => {
+      PurchaseAmountValidator.validate(Number("1000won"));
+    }).toThrow(ERROR.INVALID_AMOUNT_NOT_NUMBER);
+  });
+
+  test("로또 구입 금액이 1,000원 미만일 경우 예외가 발생한다.", () => {
+    expect(() => {
+      PurchaseAmountValidator.validate(999);
+    }).toThrow(ERROR.INVALID_AMOUNT_BELOW_MIN);
+  });
+
+  test("로또 구입 금액이 1,000원 단위가 아닐 경우 예외가 발생한다.", () => {
+    expect(() => {
+      PurchaseAmountValidator.validate(1300);
+    }).toThrow(ERROR.INVALID_AMOUNT_UNIT);
+  });
+});
