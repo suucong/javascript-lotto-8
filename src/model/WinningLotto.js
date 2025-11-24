dc39fe7399015ce57b57934d67071e21b16b6499
suucong <suucong2@swu.ac.kr>
2025-11-24 17:24:25 +0900

    feat(winning): 변경된 Lotto에 따라 compare 메서드 제거

--- src/model/WinningLotto.js
+++ src/model/WinningLotto.js
@@ -7,19 +7,12 @@ class WinningLotto {
     this.#bonusNumber = bonusNumber;
   }
 
-  compare(lotto) {
-    const purchasedNumbers = lotto.getNumbers();
-    let matchCount = 0;
+  getNumbers() {
+    return this.#numbers;
+  }
 
-    purchasedNumbers.forEach((number) => {
-      if (this.#numbers.includes(number)) {
-        matchCount += 1;
-      }
-    });
-
-    const hasBonus = purchasedNumbers.includes(this.#bonusNumber);
-
-    return { matchCount, hasBonus };
+  getBonusNumber() {
+    return this.#bonusNumber;
   }
 }
 
